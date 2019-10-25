3898a90c1eaaf74fc23224a841e0fa7257a294a2
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-10-25 09:16:51 +0200

    Endrer norg_endpoint_url til å ikke gå via api-gw (siden denne appen er i fss)

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/health/checks/NorgCheck.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/health/checks/NorgCheck.kt
@@ -29,18 +29,16 @@ class NorgCheck(private val restTemplate: RestTemplate,
 
     override fun doCheck() {
         try {
-            val norgApiKey = System.getenv("NORG_PASSWORD")
             val headers = HttpHeaders()
             headers.set("Nav-Call-Id", generateCallId())
-            headers.set("x-nav-apiKey", norgApiKey)
 
             // samme kall som selftest i soknad-api
             restTemplate.exchange("$address/kodeverk/EnhetstyperNorg", HttpMethod.GET, HttpEntity<Nothing>(headers), String::class.java)
         } catch (e: HttpStatusCodeException) {
-            log.warn("Selftest - noe feilet ved kall mot NORG - ${e.statusCode} ${e.statusText}", e)
+            log.warn("Selftest - Norg2 - noe feilet - ${e.statusCode} ${e.statusText}", e)
             throw NorgException(e.statusCode, e.message, e)
         } catch (e: Exception) {
-            log.warn("Selftest - noe feilet ved kall mot NORG", e)
+            log.warn("Selftest - Norg2 - noe feilet", e)
             throw NorgException(null, e.message, e)
         }
     }
--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/norg/NorgClientImpl.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/norg/NorgClientImpl.kt
@@ -20,30 +20,28 @@ import org.springframework.web.client.RestTemplate
 class NorgClientImpl(clientProperties: ClientProperties,
                      private val restTemplate: RestTemplate) : NorgClient {
 
-    companion object {
-        val log by logger()
-    }
-
     private val baseUrl = clientProperties.norgEndpointUrl
 
     override fun hentNavEnhet(enhetsnr: String): NavEnhet {
-        val norgApiKey = System.getenv("NORG_PASSWORD")
         val headers = HttpHeaders()
         headers.set("Nav-Call-Id", generateCallId())
-        headers.set("x-nav-apiKey", norgApiKey)
         try {
-            log.info("Forsøker å hente NAV-enhet $enhetsnr fra NORG2")
+            log.info("Norg2 - GET enhet $enhetsnr")
             val response = restTemplate.exchange("$baseUrl/enhet/$enhetsnr", HttpMethod.GET, HttpEntity<Nothing>(headers), String::class.java)
 
-            log.info("Hentet NAV-enhet $enhetsnr fra NORG2")
+            log.info("Norg2 - GET enhet OK")
             return objectMapper.readValue(response.body!!, NavEnhet::class.java)
 
         } catch (e: HttpStatusCodeException) {
-            log.warn("Noe feilet ved kall mot NORG - ${e.statusCode} ${e.statusText}", e)
+            log.warn("Norg2 - Noe feilet - ${e.statusCode} ${e.statusText}", e)
             throw NorgException(e.statusCode, e.message, e)
         } catch (e: Exception) {
-            log.warn("Noe feilet ved kall mot NORG", e)
+            log.warn("Norg2 - Noe feilet", e)
             throw NorgException(null, e.message, e)
         }
+    }
+
+    companion object {
+        val log by logger()
     }
 }
