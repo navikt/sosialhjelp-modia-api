13ed0a429d034ad083cada551a1af4cd2dc118b1
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-10-27 12:55:15 +0100

    trenger ikke multipartconfig for modia-api

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/common/Exceptions.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/common/Exceptions.kt
@@ -2,8 +2,6 @@ package no.nav.sbl.sosialhjelpmodiaapi.common
 
 import org.springframework.http.HttpStatus
 
-class InvalidInputException(message: String) : Exception(message)
-
 class FiksException(status: HttpStatus?, override val message: String?, override val cause: Throwable?): RuntimeException(message, cause)
 
 class NorgException(status: HttpStatus?, override val message: String?, override val cause: Throwable?): RuntimeException(message,cause)
