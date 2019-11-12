6049a2f96b11c05916c036aac719a5fa1163324e
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-11-12 13:48:52 +0100

    Oppgraderer spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ group = "no.nav.sbl"
 version = "1.0-SNAPSHOT"
 
 val kotlinVersion = "1.3.50"
-val springBootVersion = "2.1.9.RELEASE"
+val springBootVersion = "2.2.1.RELEASE"
 val logbackVersion = "1.2.3"
 val logstashVersion = "5.3"
 val junitJupiterVersion = "5.5.2"
@@ -31,7 +31,7 @@ plugins {
     kotlin("jvm") version "1.3.50"
 
     id("org.jetbrains.kotlin.plugin.spring") version "1.3.50"
-    id("org.springframework.boot") version "2.1.9.RELEASE"
+    id("org.springframework.boot") version "2.2.1.RELEASE"
     id("io.spring.dependency-management") version "1.0.8.RELEASE"
 }
 
