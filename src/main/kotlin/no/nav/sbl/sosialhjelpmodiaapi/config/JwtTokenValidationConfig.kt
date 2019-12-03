9e1f3dd35f9e9cf51815a533fe5a97f31772b2fc
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-12-03 19:30:47 +0100

    midlertidig skrur av tokenvalidering

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/config/JwtTokenValidationConfig.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/config/JwtTokenValidationConfig.kt
@@ -1,8 +1,7 @@
 package no.nav.sbl.sosialhjelpmodiaapi.config
 
-import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableJwtTokenValidation(ignore=["org.springframework", "springfox.documentation.swagger.web.ApiResourceController"])
+//@EnableJwtTokenValidation(ignore=["org.springframework", "springfox.documentation.swagger.web.ApiResourceController"])
 class JwtTokenValidationConfig
