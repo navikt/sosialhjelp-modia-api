47a1b8fcc4c20f15d923d9bee47bc1e41258a33d
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-03-16 16:16:15 +0100

    prøver å legge abac på personinfo-endepunkt

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/abac/AbacService.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/abac/AbacService.kt
@@ -1,24 +1,35 @@
 package no.nav.sbl.sosialhjelpmodiaapi.abac
 
+import no.nav.abac.xacml.NavAttributter.ENVIRONMENT_FELLES_OIDC_TOKEN_BODY
+import no.nav.abac.xacml.NavAttributter.ENVIRONMENT_FELLES_PEP_ID
+import no.nav.abac.xacml.NavAttributter.RESOURCE_FELLES_DOMENE
+import no.nav.abac.xacml.NavAttributter.RESOURCE_FELLES_PERSON_TILKNYTTET_FNR
+import no.nav.abac.xacml.NavAttributter.RESOURCE_FELLES_RESOURCE_TYPE
 import no.nav.sbl.sosialhjelpmodiaapi.common.TilgangskontrollException
 import org.springframework.stereotype.Component
 
 @Component
 class AbacService (private val client: AbacClient) {
 
-    fun harTilgang(saksbehandler: String, soker: String): Boolean {
+    fun harTilgang(soker: String, token: String): Boolean {
 
-//        val request = Request()
-//        val decision = client.sjekkTilgang(request)
-//        if (decision == Decision.Permit) {
-//            return true
-//        } else if (decision == Decision.Deny) {
-//            return false
-//        } else {
-//            throw TilgangskontrollException("Ukjent decision", null)
-//        }
-
-        return true
+        val request = Request(
+                environment = Attributes(mutableListOf(
+                        Attribute(ENVIRONMENT_FELLES_PEP_ID, "srvsosialhjelp-mod"),
+                        Attribute(ENVIRONMENT_FELLES_OIDC_TOKEN_BODY, token))),
+                action = null,
+                resource = Attributes(mutableListOf(
+                        Attribute(RESOURCE_FELLES_DOMENE, "domene for digisos"), //TODO?
+                        Attribute(RESOURCE_FELLES_RESOURCE_TYPE, "no.nav.abac.attributter.resource.sosialhjelp"),
+                        Attribute(RESOURCE_FELLES_PERSON_TILKNYTTET_FNR, soker))),
+                accessSubject = null
+        )
+        val decision = client.sjekkTilgang(request)
+        return when (decision) {
+            Decision.Permit -> true
+            Decision.Deny -> false
+            else -> throw TilgangskontrollException("Ukjent decision", null)
+        }
     }
 
     fun ping(): Boolean {
--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/personinfo/PersonInfoController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/personinfo/PersonInfoController.kt
@@ -1,5 +1,7 @@
 package no.nav.sbl.sosialhjelpmodiaapi.personinfo
 
+import no.nav.sbl.sosialhjelpmodiaapi.abac.AbacService
+import no.nav.sbl.sosialhjelpmodiaapi.common.TilgangskontrollException
 import no.nav.sbl.sosialhjelpmodiaapi.domain.PersoninfoResponse
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.HttpHeaders.AUTHORIZATION
@@ -12,11 +14,19 @@ import org.springframework.web.bind.annotation.RestController
 @ProtectedWithClaims(issuer = "veileder")
 @RestController
 @RequestMapping("/api/v1/innsyn")
-class PersonInfoController(private val personinfoService: PersoninfoService) {
+class PersonInfoController(private val personinfoService: PersoninfoService,
+                           private val abacService: AbacService) {
 
     @GetMapping("/personinfo", produces = ["application/json;charset=UTF-8"])
     fun hentPersonInfo(@RequestHeader(value = AUTHORIZATION) token: String): ResponseEntity<PersoninfoResponse> {
-        val personinfoResponse = personinfoService.hentPersoninfo("ident")
+        // hardkodet testbruker som saksbehandler med tilgang
+        val testIdentSaksbehandler = "Z994099"
+        val testbrukerNatalie = "26104500284"
+
+        if (!abacService.harTilgang(testbrukerNatalie, token)) {
+            throw TilgangskontrollException("ingen tilgang til ressurs", null)
+        }
+        val personinfoResponse = personinfoService.hentPersoninfo(testbrukerNatalie)
         return ResponseEntity.ok(personinfoResponse)
     }
 }
