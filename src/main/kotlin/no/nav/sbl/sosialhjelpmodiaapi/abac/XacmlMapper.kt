4b63a6b4885fbf3d7da7a624b6698b762889c504
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-03-19 14:20:44 +0100

    endrer jackson property

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/abac/XacmlMapper.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/abac/XacmlMapper.kt
@@ -10,7 +10,7 @@ object XacmlMapper {
     private val objectMapper = ObjectMapper()
             .registerKotlinModule()
             .registerModule(JavaTimeModule())
-            .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+            .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, false)
             .configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
