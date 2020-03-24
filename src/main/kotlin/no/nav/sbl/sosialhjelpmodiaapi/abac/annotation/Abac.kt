7009547a9fbd1819c7061d7dee640c8461563635
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-03-24 15:16:15 +0100

    annotation mangler groups og payload

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/abac/annotation/Abac.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/abac/annotation/Abac.kt
@@ -1,6 +1,8 @@
 package no.nav.sbl.sosialhjelpmodiaapi.abac.annotation
 
 import javax.validation.Constraint
+import javax.validation.Payload
+import kotlin.reflect.KClass
 
 
 @MustBeDocumented
@@ -8,5 +10,7 @@ import javax.validation.Constraint
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.VALUE_PARAMETER)
 annotation class Abac(
-        val message: String = "Ikke tilgang til Modia Sosialhjelp"
+        val message: String = "Ikke tilgang til Modia Sosialhjelp",
+        val groups: Array<KClass<*>> = [],
+        val payload: Array<KClass<out Payload>> = []
 )
