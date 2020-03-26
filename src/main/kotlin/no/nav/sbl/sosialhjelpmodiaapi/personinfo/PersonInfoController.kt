24235e8a8b85cac1678949ac8b490ac7c476bdb0
martintveter <41987225+martintveter@users.noreply.github.com>
2020-03-26 13:12:55 +0100

    Endrer GET-kall til POST-kall med fnr i body (#8)
    * endrer endepunkter som krever tilgangskontroll-sjekk (abac) til POST-kall som tar inn brukers fnr som body

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/personinfo/PersonInfoController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/personinfo/PersonInfoController.kt
@@ -1,22 +1,25 @@
 package no.nav.sbl.sosialhjelpmodiaapi.personinfo
 
+import no.nav.sbl.sosialhjelpmodiaapi.domain.Ident
 import no.nav.sbl.sosialhjelpmodiaapi.domain.PersoninfoResponse
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @ProtectedWithClaims(issuer = "veileder")
 @RestController
-@RequestMapping("/api/v1/innsyn")
+@RequestMapping("/api/v1/innsyn", produces = ["application/json;charset=UTF-8"], consumes = ["application/json;charset=UTF-8"])
 class PersonInfoController(private val personinfoService: PersoninfoService) {
 
-    @GetMapping("/personinfo", produces = ["application/json;charset=UTF-8"])
-    fun hentPersonInfo(@RequestHeader(value = AUTHORIZATION) token: String): ResponseEntity<PersoninfoResponse> {
-        val personinfoResponse = personinfoService.hentPersoninfo("ident")
+    @PostMapping("/personinfo")
+    fun hentPersonInfo(@RequestHeader(value = AUTHORIZATION) token: String, @RequestBody ident: Ident): ResponseEntity<PersoninfoResponse> {
+        // sjekk tilgang til fnr
+        val personinfoResponse = personinfoService.hentPersoninfo(ident.fnr)
         return ResponseEntity.ok(personinfoResponse)
     }
 }
--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/soknadsstatus/SoknadsStatusController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/soknadsstatus/SoknadsStatusController.kt
@@ -1,23 +1,26 @@
 package no.nav.sbl.sosialhjelpmodiaapi.soknadsstatus
 
+import no.nav.sbl.sosialhjelpmodiaapi.domain.Ident
 import no.nav.sbl.sosialhjelpmodiaapi.domain.SoknadsStatusResponse
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @ProtectedWithClaims(issuer = "veileder")
 @RestController
-@RequestMapping("/api/v1/innsyn/")
+@RequestMapping("/api/v1/innsyn", produces = ["application/json;charset=UTF-8"], consumes = ["application/json;charset=UTF-8"])
 class SoknadsStatusController(private val soknadsStatusService: SoknadsStatusService) {
 
-    @GetMapping("{fiksDigisosId}/soknadsStatus", produces = ["application/json;charset=UTF-8"])
-    fun hentSoknadsStatus(@PathVariable fiksDigisosId: String, @RequestHeader(value = AUTHORIZATION) token: String): ResponseEntity<SoknadsStatusResponse> {
-        // Gitt innlogget bruker
+    @PostMapping("/{fiksDigisosId}/soknadsStatus")
+    fun hentSoknadsStatus(@PathVariable fiksDigisosId: String, @RequestHeader(value = AUTHORIZATION) token: String, @RequestBody ident: Ident): ResponseEntity<SoknadsStatusResponse> {
+        // sjekk tilgang til fnr
+        // kan ikke bruke saksbehandlers token til å hente soknadsStatus?
         val soknadsStatus: SoknadsStatusResponse = soknadsStatusService.hentSoknadsStatus(fiksDigisosId, token)
         return ResponseEntity.ok().body(soknadsStatus)
     }
