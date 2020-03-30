b4e21f5cd2a127b46902bf8c4d966e0323b01db8
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-03-30 12:12:57 +0200

    enabler swagger i alle miljøer unntatt prod

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/mock/SwaggerConfig.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/mock/SwaggerConfig.kt
@@ -2,6 +2,7 @@ package no.nav.sbl.sosialhjelpmodiaapi.mock
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import springfox.documentation.builders.PathSelectors
@@ -10,7 +11,7 @@ import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
-//@Profile("mock")
+@Profile("!prod-fss")
 @Configuration
 @EnableSwagger2
 class SwaggerConfig : WebMvcConfigurer {
