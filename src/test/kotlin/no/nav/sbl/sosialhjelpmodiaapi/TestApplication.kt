bf302c6928ceb0d043625f10979c9765e915484d
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-04-21 15:45:35 +0200

    setter portnr for mock og gcp

--- src/test/kotlin/no/nav/sbl/sosialhjelpmodiaapi/TestApplication.kt
+++ src/test/kotlin/no/nav/sbl/sosialhjelpmodiaapi/TestApplication.kt
@@ -10,6 +10,23 @@ import org.springframework.context.annotation.Import
 @Import(TokenGeneratorConfiguration::class)
 class TestApplication
 
+private const val DEFAULT_PORT = 8383
+
 fun main(args: Array<String>) {
-    runApplication<TestApplication>(*args).registerShutdownHook()
+    runApplication<TestApplication>(*args)
+    {
+        setDefaultProperties(mapOf("server.port" to getPort()))
+        setRegisterShutdownHook(true)
+    }
+}
+
+private fun getPort(): Int {
+    if (isRunningOnGCP()) {
+        return Integer.parseInt(System.getenv("GCP_PORT"))
+    }
+    return DEFAULT_PORT
+}
+
+private fun isRunningOnGCP(): Boolean {
+    return System.getenv().containsKey("GCP_PORT")
 }
