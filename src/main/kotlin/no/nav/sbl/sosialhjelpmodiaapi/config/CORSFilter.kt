a84e41b1311ffdf5e324fc3d983ce35b3a48ecbb
siljee <silje.hansen3@nav.no>
2020-05-06 12:11:19 +0200

    Klargjøre CORS-filteret for at frontenden kan sende med headeren call… (#14)
    * Klargjøre CORS-filteret for at frontenden kan sende med headeren call-id.

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/config/CORSFilter.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/config/CORSFilter.kt
@@ -28,7 +28,7 @@ class CORSFilter : Filter {
 
         if (!isRunningInProd() || ALLOWED_ORIGINS.contains(origin)) {
             httpResponse.setHeader("Access-Control-Allow-Origin", origin)
-            httpResponse.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-XSRF-TOKEN, Authorization")
+            httpResponse.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-XSRF-TOKEN, Authorization, Nav-Call-Id")
             httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
             httpResponse.setHeader("Access-Control-Allow-Credentials", "true")
         }
