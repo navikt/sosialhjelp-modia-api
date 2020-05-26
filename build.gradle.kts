a96acdb15496362d8188011ee1f668b14f7366bb
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-05-26 09:58:50 +0200

    spring boot upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "no.nav.sbl"
 
 object Versions {
-    const val kotlin = "1.3.70"
-    const val springBoot = "2.2.7.RELEASE"
+    const val kotlin = "1.3.72"
+    const val springBoot = "2.3.0.RELEASE"
     const val logback = "1.2.3"
     const val logstash = "6.3"
     const val filformat = "1.2020.01.09-15.55-f18d10d7d76a"
-    const val micrometerRegistry = "1.3.8"
+    const val micrometerRegistry = "1.5.1"
     const val prometheus = "0.8.1"
     const val tokenValidation = "1.1.5"
     const val jackson = "2.11.0"
@@ -34,11 +34,10 @@ val mainClass = "no.nav.sbl.sosialhjelpmodiaapi.ApplicationKt"
 
 plugins {
     application
-    kotlin("jvm") version "1.3.70"
+    kotlin("jvm") version "1.3.72"
 //    id("org.jmailen.kotlinter") version "2.3.1" // TODO - burde tas i bruk
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.70"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.72"
     id("com.github.johnrengelman.shadow") version "5.2.0"
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("com.github.ben-manes.versions") version "0.28.0"
 }
 
