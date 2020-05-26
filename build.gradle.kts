b184af16d3c1eae0e515e4fd7b0bb4427ac158d5
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-05-26 11:13:01 +0200

    spring-boot-starter-validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,6 +82,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security:${Versions.springBoot}")
     implementation("org.springframework.boot:spring-boot-starter-actuator:${Versions.springBoot}")
     implementation("org.springframework.boot:spring-boot-starter-logging:${Versions.springBoot}")
+    implementation("org.springframework.boot:spring-boot-starter-validation:${Versions.springBoot}")
 
     implementation("io.micrometer:micrometer-registry-prometheus:${Versions.micrometerRegistry}")
     implementation("io.prometheus:simpleclient_common:${Versions.prometheus}")
