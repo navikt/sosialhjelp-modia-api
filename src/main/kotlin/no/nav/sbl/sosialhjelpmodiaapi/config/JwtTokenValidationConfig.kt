9053e0a1f78ae595dfb838194d97f36cd88d73fd
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-06-29 16:17:49 +0200

    enabler token-validation på endepunktene

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/config/JwtTokenValidationConfig.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/config/JwtTokenValidationConfig.kt
@@ -4,7 +4,7 @@ import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
-@Profile("!mock")
+@Profile("!(mock | mock-alt")
 @Configuration
-//@EnableJwtTokenValidation(ignore=["org.springframework", "springfox.documentation.swagger.web.ApiResourceController"])
+@EnableJwtTokenValidation(ignore=["org.springframework", "springfox.documentation.swagger.web.ApiResourceController"])
 class JwtTokenValidationConfig
