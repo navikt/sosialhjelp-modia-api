19c047610fc6959374106e0a4303f927c747775c
siljee <silje.hansen3@nav.no>
2020-07-03 10:24:28 +0200

    Fikse lokal kjøring og exception håndtering av token-validation * Fikse token-validation ved kjøring av TeasApplication med profiel local * Legge til exceptionHandling av token-validation

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/common/ExceptionHandler.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/common/ExceptionHandler.kt
@@ -1,15 +1,25 @@
 package no.nav.sbl.sosialhjelpmodiaapi.common
 
 import no.nav.sbl.sosialhjelpmodiaapi.logger
+import no.nav.security.token.support.core.exceptions.IssuerConfigurationException
+import no.nav.security.token.support.core.exceptions.JwtTokenMissingException
+import no.nav.security.token.support.core.exceptions.MetaDataNotAvailableException
+import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 
 @ControllerAdvice
 class ExceptionHandler : ResponseEntityExceptionHandler() {
+
+    @Value("\${loginurl}")
+    private val loginurl: String? = null
 
     @ExceptionHandler(Throwable::class)
     fun handleAll(e: Throwable): ResponseEntity<FrontendErrorMessage> {
@@ -53,6 +63,34 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
         return ResponseEntity(error, HttpStatus.FORBIDDEN)
     }
 
+    @ExceptionHandler(value = [JwtTokenUnauthorizedException::class, JwtTokenMissingException::class])
+    fun handleTokenValidationExceptions(
+            ex: RuntimeException, request: WebRequest): ResponseEntity<FrontendErrorMessage> {
+        if (ex.message?.contains("Server misconfigured") == true) {
+            log.error(ex.message)
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(FrontendErrorMessage("unexpected_error", "Noe uventet feilet"))
+        }
+        log.info("Bruker er ikke autentisert mot AzureAD (enda). Sender 401 med loginurl. Feilmelding: ${ex.message}")
+        return createUnauthorizedWithLoginUrlResponse(loginurl!!)
+    }
+
+    @ExceptionHandler(value = [MetaDataNotAvailableException::class, IssuerConfigurationException::class])
+    fun handleTokenValidationConfigurationExceptions(
+            ex: RuntimeException, request: WebRequest): ResponseEntity<FrontendErrorMessage> {
+        log.error("Klarer ikke hente metadata fra discoveryurl eller problemer ved konfigurering av issuer. Feilmelding: ${ex.message}")
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(FrontendErrorMessage("unexpected_error", "Noe uventet feilet"))
+    }
+
+    private fun createUnauthorizedWithLoginUrlResponse(loginUrl: String): ResponseEntity<FrontendErrorMessage> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(FrontendUnauthorizedMelding(loginUrl, "authentication_error", "Autentiseringsfeil"))
+    }
+
     companion object {
         private val log by logger()
 
@@ -63,8 +101,14 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
         private const val INGEN_TILGANG = "ingen_tilgang"
     }
 
-    data class FrontendErrorMessage(
+    open class FrontendErrorMessage(
             val type: String?,
             val message: String?
     )
+
+    class FrontendUnauthorizedMelding(
+            val loginUrl: String,
+            type: String?,
+            message: String?)
+        : FrontendErrorMessage(type, message)
 }
