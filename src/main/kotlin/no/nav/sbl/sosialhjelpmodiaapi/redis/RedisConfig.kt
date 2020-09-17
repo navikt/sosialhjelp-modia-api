b42a1adb8f0fc4b165d60c76382cd87dd4ada65d
siljee <silje.hansen3@nav.no>
2020-09-17 11:48:07 +0200

    Logge antall tegn i redis-passord

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/redis/RedisConfig.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/redis/RedisConfig.kt
@@ -2,6 +2,7 @@ package no.nav.sbl.sosialhjelpmodiaapi.redis
 
 import io.lettuce.core.RedisClient
 import io.lettuce.core.RedisURI
+import no.nav.sbl.sosialhjelpmodiaapi.logger
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
@@ -18,9 +19,17 @@ class RedisConfig(
         cacheProperties.startInMemoryRedisIfMocked()
 
         val redisUri = RedisURI.create(properties.host, properties.port)
+        if (properties.password == null) {
+            log.info("redis.password er null");
+        } else {
+            log.info("redis.password har så mange tegn: " + properties.password.length);
+        }
         redisUri.setPassword(properties.password)
 
         return RedisClient.create(redisUri)
     }
 
+    companion object {
+        private val log by logger()
+    }
 }
