428bfe6c03f4810e0e18005de6f854b44dc64e89
martintveter <41987225+martintveter@users.noreply.github.com>
2020-09-22 14:23:41 +0200

    Feature/idporten accesstoken gjenbrukes (#74)
    * IdPortenService holder på accessToken fra IdPorten, inntil 10sek før token utløper

---
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/service/idporten/IdPortenService.kt
@@ -0,0 +1,48 @@
+package no.nav.sbl.sosialhjelpmodiaapi.service.idporten
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import no.nav.sbl.sosialhjelpmodiaapi.service.idporten.IdPortenService.CachedToken.Companion.shouldRenewToken
+import no.nav.sosialhjelp.idporten.client.AccessToken
+import no.nav.sosialhjelp.idporten.client.IdPortenClient
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+
+@Component
+class IdPortenService(
+        private val idPortenClient: IdPortenClient
+) {
+
+    private var cachedToken: CachedToken? = null
+
+    fun getToken(): AccessToken {
+        if (shouldRenewToken(cachedToken)) {
+            val tidspunktForHenting: LocalDateTime = LocalDateTime.now()
+            return runBlocking(Dispatchers.IO) { idPortenClient.requestToken() }
+                    .also { cachedToken = CachedToken(it, tidspunktForHenting) }
+        }
+
+        return cachedToken!!.accessToken
+    }
+
+    private data class CachedToken(
+            val accessToken: AccessToken,
+            val created: LocalDateTime
+    ) {
+        // 10 sek buffer fra expiresIn
+        val expirationTime: LocalDateTime = LocalDateTime.now().plusSeconds(accessToken.expiresIn - 10L)
+
+        companion object {
+            fun shouldRenewToken(token: CachedToken?): Boolean {
+                if (token == null) {
+                    return true
+                }
+                return isExpired(token)
+            }
+
+            private fun isExpired(token: CachedToken): Boolean {
+                return token.expirationTime.isBefore(LocalDateTime.now())
+            }
+        }
+    }
+}
