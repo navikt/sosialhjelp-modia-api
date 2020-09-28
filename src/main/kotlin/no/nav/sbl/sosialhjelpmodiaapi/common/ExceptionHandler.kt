95c07a12e60c26bcb5e9177fe308ea311cb235fd
siljee <silje.hansen3@nav.no>
2020-09-28 15:32:06 +0200

    Feature/oidc auth proxy (#78)
    * La til inbound application slik at oidc-auth-proxy får lov til å hente on-behalf-of token scopet til modia-api sin clientId.
    * La på logger ved Exception (for å se om det feiler på backend eller i proxy)

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/common/ExceptionHandler.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/common/ExceptionHandler.kt
@@ -51,7 +51,7 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(PdlException::class)
     fun handlePdlError(e: PdlException): ResponseEntity<FrontendErrorMessage> {
-//        log.error("Noe feilet ved kall til Pdl", e)
+        log.error("Noe feilet ved kall til Pdl", e)
         val error = FrontendErrorMessage(PDL_ERROR, "Noe uventet feilet")
         return ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR)
     }
@@ -71,7 +71,7 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(ManglendeModiaSosialhjelpTilgangException::class)
     fun handleManglendeModiaSosialhjelpTilgangException(e: ManglendeModiaSosialhjelpTilgangException): ResponseEntity<FrontendErrorMessage> {
-        // "veileder manger ad-rolle for å kunne bruke modia sosialhjelp"
+        log.info("Veileder manger ad-rolle for tilgang til sosialhjelp i modia.")
         val error = FrontendErrorMessage(TILGANG_ERROR, "Mangler tilgang")
         return ResponseEntity(error, HttpStatus.FORBIDDEN)
     }
