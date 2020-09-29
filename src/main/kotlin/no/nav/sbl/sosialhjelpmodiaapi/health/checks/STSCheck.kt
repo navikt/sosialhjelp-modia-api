aaf5fed91d548c9038280579016704c53b5faa26
Per-Christian Nielsen <55442993+perchrnie@users.noreply.github.com>
2020-09-29 15:30:01 +0200

    Legger til profil for å videresende sporingsheadere for istio sporing (jaeger). (#82)
    * Legger til profil for å vidresende sporingsheadere for istio sporing (jaeger).
    
    * Opprydding!

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/health/checks/STSCheck.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/health/checks/STSCheck.kt
@@ -2,10 +2,12 @@ package no.nav.sbl.sosialhjelpmodiaapi.health.checks
 
 import no.nav.sbl.sosialhjelpmodiaapi.config.ClientProperties
 import no.nav.sbl.sosialhjelpmodiaapi.logger
+import no.nav.sbl.sosialhjelpmodiaapi.utils.IntegrationUtils.forwardHeaders
 import no.nav.sosialhjelp.selftest.DependencyCheck
 import no.nav.sosialhjelp.selftest.DependencyType
 import no.nav.sosialhjelp.selftest.Importance
 import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClientException
@@ -26,7 +28,7 @@ class STSCheck(
     override fun doCheck() {
         try {
             val requestUrl = "$address/.well-known/openid-configuration"
-            restTemplate.exchange(requestUrl, HttpMethod.GET, null, String::class.java)
+            restTemplate.exchange(requestUrl, HttpMethod.GET, HttpEntity<Nothing>(forwardHeaders()), String::class.java)
         } catch (e: RestClientException) {
             log.warn("Selftest - STS - noe feilet", e)
             throw e
--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/utils/IntegrationUtils.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/utils/IntegrationUtils.kt
@@ -1,6 +1,8 @@
 package no.nav.sbl.sosialhjelpmodiaapi.utils
 
 import no.nav.sbl.sosialhjelpmodiaapi.config.ClientProperties
+import org.slf4j.MDC
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 
@@ -18,14 +20,33 @@ object IntegrationUtils {
     const val HEADER_CALL_ID = "Nav-Call-Id"
     const val HEADER_CONSUMER_TOKEN = "Nav-Consumer-Token"
 
+    const val X_REQUEST_ID = "x-request-id"
+    const val X_B3_TRACEID = "x-b3-traceid"
+    const val X_B3_SPANID = "x-b3-spanid"
+    const val X_B3_PARENTSPANID = "x-b3-parentspanid"
+    const val X_B3_SAMPLED = "x-b3-sampled"
+    const val X_B3_FLAGS = "x-b3-flags"
+    const val X_OT_SPAN_CONTEXT = "x-ot-span-context"
+
     fun fiksHeaders(clientProperties: ClientProperties, token: String): HttpHeaders {
         val headers = defaultFiksHeaders(clientProperties)
         headers.set(HttpHeaders.AUTHORIZATION, token)
         return headers
     }
 
+    fun forwardHeaders(headers: HttpHeaders = HttpHeaders()): HttpHeaders {
+        MDC.get(X_REQUEST_ID)?.let { headers.set(X_REQUEST_ID, it) }
+        MDC.get(X_B3_TRACEID)?.let { headers.set(X_B3_TRACEID, it) }
+        MDC.get(X_B3_SPANID)?.let { headers.set(X_B3_SPANID, it) }
+        MDC.get(X_B3_PARENTSPANID)?.let { headers.set(X_B3_PARENTSPANID, it) }
+        MDC.get(X_B3_SAMPLED)?.let { headers.set(X_B3_SAMPLED, it) }
+        MDC.get(X_B3_FLAGS)?.let { headers.set(X_B3_FLAGS, it) }
+        MDC.get(X_OT_SPAN_CONTEXT)?.let { headers.set(X_OT_SPAN_CONTEXT, it) }
+        return headers
+    }
+
     private fun defaultFiksHeaders(clientProperties: ClientProperties): HttpHeaders {
-        val headers = HttpHeaders()
+        val headers = forwardHeaders()
         headers.accept = listOf(MediaType.APPLICATION_JSON)
         headers.set(HEADER_INTEGRASJON_ID, clientProperties.fiksIntegrasjonId)
         headers.set(HEADER_INTEGRASJON_PASSORD, clientProperties.fiksIntegrasjonpassord)
