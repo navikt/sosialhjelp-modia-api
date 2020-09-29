7902853add6a316abfc62367972de7cc5dc69cf2
siljee <silje.hansen3@nav.no>
2020-09-29 12:28:28 +0200

    Returnere kontorinfo som json-object

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/rest/NavKontorinfoController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/rest/NavKontorinfoController.kt
@@ -1,6 +1,7 @@
 package no.nav.sbl.sosialhjelpmodiaapi.rest
 
 import no.nav.sbl.sosialhjelpmodiaapi.client.norg.NorgClient
+import no.nav.sbl.sosialhjelpmodiaapi.domain.KontorinfoResponse
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
@@ -12,17 +13,17 @@ import org.springframework.web.bind.annotation.RestController
 
 @ProtectedWithClaims(issuer = "azuread")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api", produces = ["application/json;charset=UTF-8"])
 class NavKontorinfoController(
         private val norgClient: NorgClient
 ) {
 
     @GetMapping("/kontorinfo", produces = ["application/json;charset=UTF-8"])
-    fun hentPersonInfo(@RequestParam(name = "enhetsnr") enhetsnr: String, @RequestHeader(value = HttpHeaders.AUTHORIZATION) token: String): ResponseEntity<String> {
+    fun hentPersonInfo(@RequestParam(name = "enhetsnr") enhetsnr: String, @RequestHeader(value = HttpHeaders.AUTHORIZATION) token: String): ResponseEntity<KontorinfoResponse> {
         val enhet = norgClient.hentNavEnhet(enhetsnr)
         if (enhet.sosialeTjenester.isNullOrBlank()) {
             return ResponseEntity.noContent().build()
         }
-        return ResponseEntity.ok(enhet.sosialeTjenester)
+        return ResponseEntity.ok(KontorinfoResponse(enhet.sosialeTjenester))
     }
 }
