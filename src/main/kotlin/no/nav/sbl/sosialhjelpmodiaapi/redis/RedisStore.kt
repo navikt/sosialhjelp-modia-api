b4b2529bf095523e4dbaac5999802b2919635d43
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-10-15 15:02:51 +0200

    formattering

--- src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/redis/RedisStore.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpmodiaapi/redis/RedisStore.kt
@@ -2,19 +2,22 @@ package no.nav.sbl.sosialhjelpmodiaapi.redis
 
 import io.lettuce.core.RedisClient
 import io.lettuce.core.RedisFuture
+import io.lettuce.core.api.StatefulRedisConnection
+import io.lettuce.core.api.async.RedisAsyncCommands
 import io.lettuce.core.codec.ByteArrayCodec
 import io.lettuce.core.codec.RedisCodec
 import io.lettuce.core.codec.StringCodec
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
 
 
 @Component
-class RedisStore @Autowired constructor(redisClient: RedisClient) {
+class RedisStore(
+        redisClient: RedisClient
+) {
 
-    private final val connection = redisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE))
-    private val async = connection.async()!!
+    private val connection: StatefulRedisConnection<String, ByteArray> = redisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE))
+    private val async: RedisAsyncCommands<String, ByteArray> = connection.async()!!
 
     fun get(key: String): ByteArray? {
         val get: RedisFuture<ByteArray> = async.get(key)
