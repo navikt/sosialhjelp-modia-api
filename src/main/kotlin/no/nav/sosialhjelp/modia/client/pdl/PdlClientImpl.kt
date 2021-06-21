b201df42f6a9c755056312616def9a13e40b5d95
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-06-21 14:21:02 +0200

    pdl: resttemplate -> webclient

--- src/main/kotlin/no/nav/sosialhjelp/modia/client/pdl/PdlClientImpl.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/client/pdl/PdlClientImpl.kt
@@ -2,7 +2,6 @@ package no.nav.sosialhjelp.modia.client.pdl
 
 import no.nav.sosialhjelp.modia.client.sts.STSClient
 import no.nav.sosialhjelp.modia.common.PdlException
-import no.nav.sosialhjelp.modia.config.ClientProperties
 import no.nav.sosialhjelp.modia.logger
 import no.nav.sosialhjelp.modia.utils.IntegrationUtils.BEARER
 import no.nav.sosialhjelp.modia.utils.IntegrationUtils.HEADER_CALL_ID
@@ -11,62 +10,54 @@ import no.nav.sosialhjelp.modia.utils.IntegrationUtils.HEADER_TEMA
 import no.nav.sosialhjelp.modia.utils.IntegrationUtils.TEMA_KOM
 import no.nav.sosialhjelp.modia.utils.mdc.MDCUtils.getCallId
 import org.springframework.context.annotation.Profile
-import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpHeaders.AUTHORIZATION
-import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestClientException
-import org.springframework.web.client.RestClientResponseException
-import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import org.springframework.web.reactive.function.client.bodyToMono
 
 @Profile("!(mock | local)")
 @Component
 class PdlClientImpl(
-    clientProperties: ClientProperties,
-    private val restTemplate: RestTemplate,
+    private val pdlWebClient: WebClient,
     private val stsClient: STSClient
 ) : PdlClient {
 
-    private val baseurl = clientProperties.pdlEndpointUrl
-
     override fun hentPerson(ident: String): PdlHentPerson? {
-        val query = getResourceAsString("/pdl/hentPerson.graphql").replace("[\n\r]", "")
-        try {
-            val requestEntity = createRequestEntity(PdlRequest(query, Variables(ident)))
-            val response = restTemplate.exchange(baseurl, HttpMethod.POST, requestEntity, PdlPersonResponse::class.java)
+        val query = getResourceAsString("/pdl/hentPerson.graphql")
 
-            val pdlPersonResponse: PdlPersonResponse = response.body!!
-            if (pdlPersonResponse.errors != null && pdlPersonResponse.errors.isNotEmpty()) {
-                pdlPersonResponse.errors
-                    .forEach { log.error("PDL - noe feilet. Message=${it.message}, path=${it.path}, code=${it.extensions.code}, classification=${it.extensions.classification}") }
-                val firstError = pdlPersonResponse.errors[0]
-                val statusCode = firstError.extensions.code?.uppercase()?.let { HttpStatus.valueOf(it) }
-                throw PdlException(
-                    "StatusCode: $statusCode, Message: ${firstError.message}, Classification: ${firstError.extensions.classification}"
-                )
+        val pdlPersonResponse = pdlWebClient.post()
+            .headers { it.addAll(headers()) }
+            .bodyValue(PdlRequest(query, Variables(ident)))
+            .retrieve()
+            .bodyToMono<PdlPersonResponse>()
+            .onErrorMap(WebClientResponseException::class.java) {
+                log.error("PDL - ${it.rawStatusCode} ${it.statusText} feil ved henting av navn fra PDL", it)
+                PdlException(it.message)
             }
-            return pdlPersonResponse.data
-        } catch (e: RestClientResponseException) {
-            log.error("PDL - ${e.rawStatusCode} ${e.statusText} feil ved henting av navn, requesturl: $baseurl", e)
-            throw PdlException(e.message)
-        }
+            .block()
+
+        checkForPdlApiErrors(pdlPersonResponse)
+
+        return pdlPersonResponse?.data
     }
 
     override fun ping() {
-        try {
-            restTemplate.exchange(baseurl, HttpMethod.OPTIONS, HttpEntity(null, null), String::class.java)
-        } catch (e: RestClientException) {
-            log.error("PDL - ping feilet, requesturl: $baseurl", e)
-            throw e
-        }
+        pdlWebClient.options()
+            .retrieve()
+            .bodyToMono<String>()
+            .doOnError { e ->
+                log.error("PDL - ping feilet", e)
+            }
+            .block()
     }
 
-    private fun getResourceAsString(path: String) = this::class.java.getResource(path).readText()
+    private fun getResourceAsString(path: String) = this.javaClass.getResource(path)?.readText()?.replace("[\n\r]", "")
+        ?: throw RuntimeException("Feil ved lesing av graphql-spørring fra fil")
 
-    private fun createRequestEntity(request: PdlRequest): HttpEntity<PdlRequest> {
+    private fun headers(): HttpHeaders {
         val stsToken: String = stsClient.token()
 
         val headers = HttpHeaders()
@@ -75,7 +66,18 @@ class PdlClientImpl(
         headers.set(HEADER_CONSUMER_TOKEN, BEARER + stsToken)
         headers.set(AUTHORIZATION, BEARER + stsToken)
         headers.set(HEADER_TEMA, TEMA_KOM)
-        return HttpEntity(request, headers)
+        return headers
+    }
+
+    private fun checkForPdlApiErrors(response: PdlPersonResponse?) {
+        response?.errors?.let { handleErrors(it) }
+    }
+
+    private fun handleErrors(errors: List<PdlError>) {
+        val errorString: String = errors
+            .map { it.message + "(feilkode: " + it.extensions.code + ")" }
+            .joinToString(prefix = "Error i respons fra pdl-api: ") { it }
+        throw PdlException(errorString)
     }
 
     companion object {
