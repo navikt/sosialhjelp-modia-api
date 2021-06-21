b2316f7cc674a148853bb4a473828ba92e4ea675
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-06-21 09:41:07 +0200

    digisosapiclient: resttemplate -> webclient

--- src/test/kotlin/no/nav/sosialhjelp/modia/client/digisosapi/DigisosApiClientTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/modia/client/digisosapi/DigisosApiClientTest.kt
@@ -2,39 +2,35 @@ package no.nav.sosialhjelp.modia.client.digisosapi
 
 import io.mockk.every
 import io.mockk.mockk
+import mockwebserver3.MockResponse
+import mockwebserver3.MockWebServer
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.JsonDigisosSoker
 import no.nav.sosialhjelp.idporten.client.AccessToken
 import no.nav.sosialhjelp.modia.config.ClientProperties
-import no.nav.sosialhjelp.modia.responses.ok_komplett_jsondigisossoker_response
 import no.nav.sosialhjelp.modia.service.idporten.IdPortenService
 import no.nav.sosialhjelp.modia.utils.DigisosApiWrapper
 import no.nav.sosialhjelp.modia.utils.SakWrapper
 import org.junit.jupiter.api.Test
-import org.springframework.http.ResponseEntity
-import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
 
 internal class DigisosApiClientTest {
     private val clientProperties: ClientProperties = mockk(relaxed = true)
 
     @Test
     fun `Post digisos sak til mock`() {
-        val restTemplate: RestTemplate = mockk()
+        val mockWebServer = MockWebServer()
+        val fiksWebClient = WebClient.create(mockWebServer.url("/").toString())
         val idPortenService: IdPortenService = mockk()
 
-        val digisosApiClient = DigisosApiClientImpl(clientProperties, restTemplate, idPortenService)
+        val digisosApiClient = DigisosApiClientImpl(clientProperties, fiksWebClient, idPortenService)
 
-        val mockResponse: ResponseEntity<String> = mockk()
-        every { mockResponse.statusCode.is2xxSuccessful } returns true
-        every { mockResponse.body } returns ok_komplett_jsondigisossoker_response
         every { idPortenService.getToken() } returns AccessToken("Token", 999)
-        every {
-            restTemplate.exchange(
-                any<String>(),
-                any(),
-                any(),
-                String::class.java
-            )
-        } returns mockResponse
+
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(202)
+                .setBody("ok")
+        )
 
         digisosApiClient.oppdaterDigisosSak("123123", DigisosApiWrapper(SakWrapper(JsonDigisosSoker()), ""))
     }
