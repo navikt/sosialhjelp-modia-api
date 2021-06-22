9e35da6d419997f4b8a0859574a575b4255a4f3f
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-06-22 11:59:15 +0200

    spring

--- src/main/kotlin/no/nav/sosialhjelp/modia/client/abac/AbacConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/client/abac/AbacConfig.kt
@@ -1,10 +1,10 @@
 package no.nav.sosialhjelp.modia.client.abac
 
-import com.google.common.net.HttpHeaders
 import no.nav.sosialhjelp.modia.config.ClientProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpHeaders
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
