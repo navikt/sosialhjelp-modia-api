eb4aca65e583c38d5cbaa5e2bf0d59a9188ba2d2
martintveter <41987225+martintveter@users.noreply.github.com>
2021-06-22 14:28:21 +0200

    msgraph: resttemplate -> webclient (#175)

--- src/main/kotlin/no/nav/sosialhjelp/modia/client/msgraph/MsGraphClient.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/client/msgraph/MsGraphClient.kt
@@ -2,35 +2,28 @@ package no.nav.sosialhjelp.modia.client.msgraph
 
 import no.nav.sosialhjelp.modia.common.MsGraphException
 import no.nav.sosialhjelp.modia.utils.IntegrationUtils.BEARER
-import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestClientException
-import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 
 @Component
 class MsGraphClient(
-    private val restTemplate: RestTemplate
+    private val proxiedWebClient: WebClient
 ) {
 
     fun hentOnPremisesSamAccountName(accessToken: String): OnPremisesSamAccountName {
-        try {
-            val url = "https://graph.microsoft.com/v1.0/me?\$select=$ON_PREMISES_SAM_ACCOUNT_NAME_FIELD"
-            val headers = createRequestEntity(accessToken)
-            val response = restTemplate.exchange(url, HttpMethod.GET, headers, OnPremisesSamAccountName::class.java)
-            return response.body!!
-        } catch (e: RestClientException) {
-            throw MsGraphException("MsGraph hentOnPremisesSamAccountName feilet", e)
-        }
-    }
-
-    private fun createRequestEntity(accessToken: String): HttpEntity<Nothing> {
-        val headers = HttpHeaders()
-        headers.set(HttpHeaders.ACCEPT, APPLICATION_JSON_VALUE)
-        headers.set(HttpHeaders.AUTHORIZATION, BEARER + accessToken)
-        return HttpEntity(headers)
+        return proxiedWebClient.get()
+            .uri("https://graph.microsoft.com/v1.0/me?\$select=$ON_PREMISES_SAM_ACCOUNT_NAME_FIELD")
+            .header(HttpHeaders.ACCEPT, APPLICATION_JSON_VALUE)
+            .header(HttpHeaders.AUTHORIZATION, BEARER + accessToken)
+            .retrieve()
+            .bodyToMono<OnPremisesSamAccountName>()
+            .onErrorMap {
+                MsGraphException("MsGraph hentOnPremisesSamAccountName feilet", it)
+            }
+            .block()!!
     }
 
     companion object {
