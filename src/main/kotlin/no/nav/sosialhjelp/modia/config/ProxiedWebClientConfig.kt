0ebe8e2e532f1e42a29a283b181911b53c8a71bb
martintveter <41987225+martintveter@users.noreply.github.com>
2021-09-29 16:16:40 +0200

    samme config som i innsyn (#182)

--- src/main/kotlin/no/nav/sosialhjelp/modia/config/ProxiedWebClientConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/config/ProxiedWebClientConfig.kt
@@ -2,10 +2,13 @@ package no.nav.sosialhjelp.modia.config
 
 import no.nav.sosialhjelp.modia.utils.getProxiedReactorClientHttpConnector
 import no.nav.sosialhjelp.modia.utils.getUnproxiedReactorClientHttpConnector
+import no.nav.sosialhjelp.modia.utils.objectMapper
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.http.codec.json.Jackson2JsonDecoder
+import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.web.reactive.function.client.WebClient
 
 @Profile("!(mock|mock-alt|local)")
@@ -19,6 +22,11 @@ class ProxiedWebClientConfig {
     fun proxiedWebClient(webClientBuilder: WebClient.Builder): WebClient =
         webClientBuilder
             .clientConnector(getProxiedReactorClientHttpConnector(proxyUrl))
+            .codecs {
+                it.defaultCodecs().maxInMemorySize(16 * 1024 * 1024)
+                it.defaultCodecs().jackson2JsonDecoder(Jackson2JsonDecoder(objectMapper))
+                it.defaultCodecs().jackson2JsonEncoder(Jackson2JsonEncoder(objectMapper))
+            }
             .build()
 }
 
