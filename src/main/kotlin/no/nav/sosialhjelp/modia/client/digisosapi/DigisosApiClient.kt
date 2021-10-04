4034355208a9343d0bf628c14bfa26a84dc30c43
martintveter <41987225+martintveter@users.noreply.github.com>
2021-10-04 10:29:44 +0200

    Fjern DigisosApiClient+DigisosApiService - ser ikke ut til å være i bruk (#186)

--- src/main/kotlin/no/nav/sosialhjelp/modia/client/digisosapi/DigisosApiClient.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.sosialhjelp.modia.client.digisosapi
-
-import no.nav.sosialhjelp.modia.utils.DigisosApiWrapper
-import org.springframework.stereotype.Component
-
-@Component
-interface DigisosApiClient {
-    fun oppdaterDigisosSak(fiksDigisosId: String?, digisosApiWrapper: DigisosApiWrapper): String?
-}
