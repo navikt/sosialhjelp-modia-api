c19746d2a9f9ffc72f23ba34d77473b674872023
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-10-05 10:10:08 +0200

    debug

--- src/main/kotlin/no/nav/sosialhjelp/modia/redis/RedisConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/redis/RedisConfig.kt
@@ -2,6 +2,7 @@ package no.nav.sosialhjelp.modia.redis
 
 import io.lettuce.core.RedisClient
 import io.lettuce.core.RedisURI
+import no.nav.sosialhjelp.modia.logger
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
@@ -16,8 +17,15 @@ class RedisConfig {
     @Bean
     fun redisClient(properties: RedisProperties): RedisClient {
         val redisUri = RedisURI.create(properties.host, properties.port)
+        if (properties.password.isNullOrEmpty()) {
+            log.error("Redis-password er nullOrEmpty??")
+        }
         redisUri.setPassword(properties.password as CharSequence)
 
         return RedisClient.create(redisUri)
+    }
+
+    companion object {
+        private val log by logger()
     }
 }
