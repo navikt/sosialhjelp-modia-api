afa6b0c11e4be7cc75423d875935b0373b8a0e21
martintveter <41987225+martintveter@users.noreply.github.com>
2021-10-14 13:28:00 +0200

    mock-alt profile krever ikke at VIRKSERT_STI er satt (#190)
    * bump sosialhjelp-common
    * bruk av mock-alt profil krever ikke lengre at VIRKSERT_STI er satt

--- src/main/kotlin/no/nav/sosialhjelp/modia/client/idporten/IdPortenClientConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/client/idporten/IdPortenClientConfig.kt
@@ -1,13 +1,20 @@
 package no.nav.sosialhjelp.modia.client.idporten
 
+import no.nav.sosialhjelp.idporten.client.AccessToken
+import no.nav.sosialhjelp.idporten.client.IdPortenAccessTokenResponse
 import no.nav.sosialhjelp.idporten.client.IdPortenClient
+import no.nav.sosialhjelp.idporten.client.IdPortenClientImpl
 import no.nav.sosialhjelp.idporten.client.IdPortenProperties
 import no.nav.sosialhjelp.modia.utils.Miljo.getVirkSertSti
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpHeaders
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
 
+@Profile("!mock-alt")
 @Configuration
 class IdPortenClientConfig(
     private val proxiedWebClient: WebClient,
@@ -19,7 +26,7 @@ class IdPortenClientConfig(
 
     @Bean
     fun idPortenClient(): IdPortenClient {
-        return IdPortenClient(
+        return IdPortenClientImpl(
             webClient = proxiedWebClient,
             idPortenProperties = idPortenProperties()
         )
@@ -33,5 +40,32 @@ class IdPortenClientConfig(
             configUrl = configUrl,
             virksomhetSertifikatPath = getVirkSertSti()
         )
+    }
+}
+
+@Profile("mock-alt")
+@Configuration
+class IdPortenClientConfigMockAlt(
+    private val proxiedWebClient: WebClient,
+    @Value("\${no.nav.sosialhjelp.idporten.token_url}") private val tokenUrl: String,
+) {
+
+    @Bean
+    fun idPortenClient(): IdPortenClient {
+        return IdPortenClientMockAlt(proxiedWebClient, tokenUrl)
+    }
+
+    private class IdPortenClientMockAlt(
+        private val proxiedWebClient: WebClient,
+        private val tokenUrl: String
+    ) : IdPortenClient {
+
+        override suspend fun requestToken(attempts: Int, headers: HttpHeaders): AccessToken {
+            val response = proxiedWebClient.post()
+                .uri(tokenUrl)
+                .retrieve()
+                .awaitBody<IdPortenAccessTokenResponse>()
+            return AccessToken(response.accessToken, response.expiresIn)
+        }
     }
 }
