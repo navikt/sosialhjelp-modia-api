18ab526ee47fcee54b66989a14c80f019fd75926
linemos <line.moseng@nav.no>
2022-03-04 14:16:35 +0100

    Bump spring boot (#210)
    * Bump spring boot
    
    * Fix deprecated constructor
    
    * ktlint

--- src/main/kotlin/no/nav/sosialhjelp/modia/utils/Objects.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/utils/Objects.kt
@@ -4,10 +4,21 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import no.nav.sbl.soknadsosialhjelp.json.JsonSosialhjelpObjectMapper
 
 val objectMapper: ObjectMapper = JsonSosialhjelpObjectMapper.createObjectMapper()
-    .registerModules(JavaTimeModule(), KotlinModule())
+    .registerModules(
+        JavaTimeModule(),
+        KotlinModule.Builder()
+            .withReflectionCacheSize(512)
+            .configure(KotlinFeature.NullToEmptyCollection, false)
+            .configure(KotlinFeature.NullToEmptyMap, false)
+            .configure(KotlinFeature.NullIsSameAsDefault, false)
+            .configure(KotlinFeature.SingletonSupport, false)
+            .configure(KotlinFeature.StrictNullChecks, false)
+            .build()
+    )
     .configure(SerializationFeature.INDENT_OUTPUT, true)
     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
