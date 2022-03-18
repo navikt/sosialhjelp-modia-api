4047f5dd00667f323d749933148532dbaf53cfc6
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-03-18 11:04:00 +0100

    Flytter srvsosialhjelp-mod til kubernetes secret

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ ENV LC_ALL="no_NB.UTF-8"
 ENV LANG="no_NB.UTF-8"
 ENV TZ="Europe/Oslo"
 
-COPY /nais/scripts /init-scripts
 COPY build/libs/*.jar app.jar
 
 ENV JAVA_OPTS="-XX:MaxRAMPercentage=75 \
--- nais/scripts/90-set-secrets.sh
+++
@@ -1,3 +0,0 @@
-echo "Export serviceuser credentials"
-export SRVSOSIALHJELP_MODIA_API_USERNAME=$(cat /serviceuser/srvsosialhjelp-mod/username)
-export SRVSOSIALHJELP_MODIA_API_PASSWORD=$(cat /serviceuser/srvsosialhjelp-mod/password)
