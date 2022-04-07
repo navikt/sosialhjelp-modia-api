48180872fc40ffd64ddd56a5c5668160068da679
Per-Christian Nielsen <55442993+perchrnie@users.noreply.github.com>
2022-04-07 15:11:49 +0200

    Legger til endepunkt for å se om veileder har tilgang til tjenesten. (#234)

---
+++ src/main/kotlin/no/nav/sosialhjelp/modia/rest/TilgangsController.kt
@@ -0,0 +1,25 @@
+package no.nav.sosialhjelp.modia.rest
+
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.sosialhjelp.modia.service.tilgangskontroll.TilgangskontrollService
+import org.springframework.http.HttpHeaders.AUTHORIZATION
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@ProtectedWithClaims(issuer = "azuread")
+@RestController
+@RequestMapping("/api")
+class TilgangsController(
+    private val tilgangskontrollService: TilgangskontrollService
+) {
+
+    @GetMapping("/tilgang")
+    fun hentPersoninfo(@RequestHeader(value = AUTHORIZATION) token: String): ResponseEntity<String> {
+        tilgangskontrollService.harVeilederTilgangTilTjenesten(token, "/tilgang", HttpMethod.GET)
+        return ResponseEntity.ok("true")
+    }
+}
