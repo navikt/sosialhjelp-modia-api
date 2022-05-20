b5d24b110471e4124dc33c50c4e0c7022e53ed4c
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-05-20 12:43:08 +0200

    add servletWebServerFactory

--- src/main/kotlin/no/nav/sosialhjelp/modia/app/SecurityConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/app/SecurityConfig.kt
@@ -2,6 +2,9 @@ package no.nav.sosialhjelp.modia.app
 
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
@@ -12,4 +15,9 @@ import org.springframework.context.annotation.Profile
 class SecurityConfig {
 
     // JwtTokenValidation er enabled så lenge appen kjører med profil != mock-alt
+
+    @Bean
+    fun servletWebServerFactory(): ServletWebServerFactory {
+        return JettyServletWebServerFactory()
+    }
 }
