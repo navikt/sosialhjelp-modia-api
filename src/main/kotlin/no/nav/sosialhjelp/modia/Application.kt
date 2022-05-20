e2b7a3ba4c2996b03860564a511f1a1faeaef241
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-05-20 12:25:57 +0200

    SpringBootServletInitializer

--- src/main/kotlin/no/nav/sosialhjelp/modia/Application.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/Application.kt
@@ -2,9 +2,10 @@ package no.nav.sosialhjelp.modia
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class Application
+class Application : SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args).registerShutdownHook()
