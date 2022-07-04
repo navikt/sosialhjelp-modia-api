aaa2ac55a7cd1837ac8280af508c9773161826f6
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-07-04 15:34:44 +0200

    webmvcconfigurer

--- src/main/kotlin/no/nav/sosialhjelp/modia/app/cors/CorsConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/app/cors/CorsConfig.kt
@@ -1,33 +1,45 @@
 package no.nav.sosialhjelp.modia.app.cors
 
 import no.nav.sosialhjelp.modia.utils.MiljoUtils
-import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Component
 class CorsConfig(
-    private val miljoUtils: MiljoUtils
+    private val miljoUtils: MiljoUtils,
 ) {
 
+//    @Bean
+//    fun corsFilter(): FilterRegistrationBean<CorsFilter> {
+//        val source = UrlBasedCorsConfigurationSource()
+//        val bean = FilterRegistrationBean<CorsFilter>()
+//        val config = CorsConfiguration()
+//        config.allowedOrigins = allowedOrigins
+//        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//        config.allowedHeaders = listOf("Origin", "Content-Type", "Accept", "X-XSRF-TOKEN", "Authorization", "Nav-Call-Id")
+//        config.allowCredentials = true
+//        config.maxAge = 3600L
+//        source.registerCorsConfiguration("/**", config)
+//        val corsFilter = CorsFilter(source)
+//        bean.filter = corsFilter
+// //        bean.order = Ordered.HIGHEST_PRECEDENCE
+//        return bean
+//    }
+
     @Bean
-    fun corsFilter(): FilterRegistrationBean<CorsFilter> {
-        val source = UrlBasedCorsConfigurationSource()
-        val bean = FilterRegistrationBean<CorsFilter>()
-        val config = CorsConfiguration()
-        config.allowedOrigins = allowedOrigins
-        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        config.allowedHeaders = listOf("Origin", "Content-Type", "Accept", "X-XSRF-TOKEN", "Authorization", "Nav-Call-Id")
-        config.allowCredentials = true
-        config.maxAge = 3600L
-        source.registerCorsConfiguration("/**", config)
-        val corsFilter = CorsFilter(source)
-        bean.filter = corsFilter
-//        bean.order = Ordered.HIGHEST_PRECEDENCE
-        return bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedOrigins(*allowedOrigins.toTypedArray())
+                    .allowedHeaders("Origin", "Content-Type", "Accept", "X-XSRF-TOKEN", "Authorization", "Nav-Call-Id")
+                    .allowCredentials(true)
+                    .maxAge(3600L)
+            }
+        }
     }
 
     private val allowedOrigins: List<String>
