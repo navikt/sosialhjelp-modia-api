68e2a1e8133a182d8c2e84133d5d1a26509e49fc
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-07-05 08:42:55 +0200

    asd

--- src/main/kotlin/no/nav/sosialhjelp/modia/app/cors/CorsConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/app/cors/CorsConfig.kt
@@ -3,6 +3,7 @@ package no.nav.sosialhjelp.modia.app.cors
 import no.nav.sosialhjelp.modia.utils.MiljoUtils
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
+import org.springframework.core.Ordered
 import org.springframework.stereotype.Component
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
@@ -26,23 +27,9 @@ class CorsConfig(
         source.registerCorsConfiguration("/**", config)
         val corsFilter = CorsFilter(source)
         bean.filter = corsFilter
-        //        bean.order = Ordered.HIGHEST_PRECEDENCE
+        bean.order = Ordered.HIGHEST_PRECEDENCE
         return bean
     }
-
-//    @Bean
-//    fun corsConfigurer(): WebMvcConfigurer {
-//        return object : WebMvcConfigurer {
-//            override fun addCorsMappings(registry: CorsRegistry) {
-//                registry.addMapping("/**")
-//                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                    .allowedOrigins(*allowedOrigins.toTypedArray())
-//                    .allowedHeaders("Origin", "Content-Type", "Accept", "X-XSRF-TOKEN", "Authorization", "Nav-Call-Id")
-//                    .allowCredentials(true)
-//                    .maxAge(3600L)
-//            }
-//        }
-//    }
 
     private val allowedOrigins: List<String>
         get() = if (isRunningInProd) ALLOWED_ORIGINS_PROD else ALLOWED_ORIGINS_NON_PROD
@@ -51,7 +38,8 @@ class CorsConfig(
 
     companion object {
         private val ALLOWED_ORIGINS_PROD = listOf(
-            "https://sosialhjelp-modia-api.intern.nav.no"
+            "https://sosialhjelp-modia-api.intern.nav.no",
+            "https://sosialhjelp-modia.intern.nav.no"
         )
         private val ALLOWED_ORIGINS_NON_PROD = listOf(
             "https://sosialhjelp-modia-api.dev.intern.nav.no",
