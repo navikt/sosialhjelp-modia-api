15194518780c4f0e958e4bc3951d4baf5e73399f
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-07-05 08:31:03 +0200

    webmvcconfigurer

--- src/main/kotlin/no/nav/sosialhjelp/modia/app/cors/CorsConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/app/cors/CorsConfig.kt
@@ -1,46 +1,48 @@
 package no.nav.sosialhjelp.modia.app.cors
 
 import no.nav.sosialhjelp.modia.utils.MiljoUtils
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 
 @Component
 class CorsConfig(
     private val miljoUtils: MiljoUtils,
 ) {
 
-//    @Bean
-//    fun corsFilter(): FilterRegistrationBean<CorsFilter> {
-//        val source = UrlBasedCorsConfigurationSource()
-//        val bean = FilterRegistrationBean<CorsFilter>()
-//        val config = CorsConfiguration()
-//        config.allowedOrigins = allowedOrigins
-//        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//        config.allowedHeaders = listOf("Origin", "Content-Type", "Accept", "X-XSRF-TOKEN", "Authorization", "Nav-Call-Id")
-//        config.allowCredentials = true
-//        config.maxAge = 3600L
-//        source.registerCorsConfiguration("/**", config)
-//        val corsFilter = CorsFilter(source)
-//        bean.filter = corsFilter
-// //        bean.order = Ordered.HIGHEST_PRECEDENCE
-//        return bean
-//    }
-
     @Bean
-    fun corsConfigurer(): WebMvcConfigurer {
-        return object : WebMvcConfigurer {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedOrigins(*allowedOrigins.toTypedArray())
-                    .allowedHeaders("Origin", "Content-Type", "Accept", "X-XSRF-TOKEN", "Authorization", "Nav-Call-Id")
-                    .allowCredentials(true)
-                    .maxAge(3600L)
-            }
-        }
+    fun corsFilter(): FilterRegistrationBean<CorsFilter> {
+        val source = UrlBasedCorsConfigurationSource()
+        val bean = FilterRegistrationBean<CorsFilter>()
+        val config = CorsConfiguration()
+        config.allowedOrigins = allowedOrigins
+        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        config.allowedHeaders = listOf("Origin", "Content-Type", "Accept", "X-XSRF-TOKEN", "Authorization", "Nav-Call-Id")
+        config.allowCredentials = true
+        config.maxAge = 3600L
+        source.registerCorsConfiguration("/**", config)
+        val corsFilter = CorsFilter(source)
+        bean.filter = corsFilter
+        //        bean.order = Ordered.HIGHEST_PRECEDENCE
+        return bean
     }
+
+//    @Bean
+//    fun corsConfigurer(): WebMvcConfigurer {
+//        return object : WebMvcConfigurer {
+//            override fun addCorsMappings(registry: CorsRegistry) {
+//                registry.addMapping("/**")
+//                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                    .allowedOrigins(*allowedOrigins.toTypedArray())
+//                    .allowedHeaders("Origin", "Content-Type", "Accept", "X-XSRF-TOKEN", "Authorization", "Nav-Call-Id")
+//                    .allowCredentials(true)
+//                    .maxAge(3600L)
+//            }
+//        }
+//    }
 
     private val allowedOrigins: List<String>
         get() = if (isRunningInProd) ALLOWED_ORIGINS_PROD else ALLOWED_ORIGINS_NON_PROD
@@ -53,8 +55,11 @@ class CorsConfig(
         )
         private val ALLOWED_ORIGINS_NON_PROD = listOf(
             "https://sosialhjelp-modia-api.dev.intern.nav.no",
+            "https://sosialhjelp-modia.dev.intern.nav.no",
             "https://sosialhjelp-modia-api-mock.dev.nav.no",
-            "https://sosialhjelp-modia-api.labs.nais.io"
+            "https://sosialhjelp-modia-mock.dev.nav.no",
+            "https://sosialhjelp-modia-api.labs.nais.io",
+            "https://sosialhjelp-modia.labs.nais.io"
         )
     }
 }
