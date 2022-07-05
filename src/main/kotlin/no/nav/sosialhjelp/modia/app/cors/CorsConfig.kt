58d6114a272548ed75b6f3f5dd922029f13c6d9d
martintveter <41987225+martintveter@users.noreply.github.com>
2022-07-05 11:04:52 +0200

    Cors configuration (#300)
    * cors config - istedet for eget filter

---
+++ src/main/kotlin/no/nav/sosialhjelp/modia/app/cors/CorsConfig.kt
@@ -0,0 +1,47 @@
+package no.nav.sosialhjelp.modia.app.cors
+
+import no.nav.sosialhjelp.modia.utils.MiljoUtils
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.core.Ordered
+import org.springframework.stereotype.Component
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Component
+class CorsConfig(
+    private val miljoUtils: MiljoUtils,
+) {
+
+    @Bean
+    fun corsFilter(): FilterRegistrationBean<CorsFilter> {
+        val source = UrlBasedCorsConfigurationSource()
+        val bean = FilterRegistrationBean<CorsFilter>()
+        val config = CorsConfiguration()
+        config.allowedOrigins = allowedOrigins
+        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        config.allowedHeaders = listOf("Origin", "Content-Type", "Accept", "X-XSRF-TOKEN", "Authorization", "Nav-Call-Id")
+        config.allowCredentials = true
+        config.maxAge = 3600L
+        source.registerCorsConfiguration("/**", config)
+        val corsFilter = CorsFilter(source)
+        bean.filter = corsFilter
+        bean.order = Ordered.HIGHEST_PRECEDENCE
+        return bean
+    }
+
+    private val allowedOrigins: List<String>
+        get() = if (miljoUtils.isRunningInProd()) ALLOWED_ORIGINS_PROD else ALLOWED_ORIGINS_NON_PROD
+
+    companion object {
+        private val ALLOWED_ORIGINS_PROD = listOf(
+            "https://sosialhjelp-modia.intern.nav.no"
+        )
+        private val ALLOWED_ORIGINS_NON_PROD = listOf(
+            "https://sosialhjelp-modia.dev.intern.nav.no",
+            "https://sosialhjelp-modia-mock.dev.nav.no",
+            "https://sosialhjelp-modia.labs.nais.io"
+        )
+    }
+}
