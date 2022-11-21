40fffefa29d151381846ec125accd355e8f4f0a3
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-11-21 12:38:55 +0100

    legger til tilgangskontroll-sjekk for POST-endepunkt for Salesforce

--- src/main/kotlin/no/nav/sosialhjelp/modia/fodselsnummer/FodselsnummerController.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/fodselsnummer/FodselsnummerController.kt
@@ -2,13 +2,17 @@ package no.nav.sosialhjelp.modia.fodselsnummer
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.sosialhjelp.modia.logger
+import no.nav.sosialhjelp.modia.tilgang.TilgangskontrollService
 import no.nav.sosialhjelp.modia.utils.Ident
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,12 +20,17 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api", produces = ["application/json;charset=UTF-8"])
 class FodselsnummerController(
+    private val tilgangskontrollService: TilgangskontrollService,
     private val fodselsnummerService: FodselsnummerService,
     @Value("\${modia_baseurl}") private val modiaBaseurl: String
 ) {
 
     @PostMapping("/fodselsnummer")
-    fun setFodselsnummer(@RequestBody ident: Ident): ResponseEntity<*> {
+    fun setFodselsnummer(
+        @RequestHeader(value = HttpHeaders.AUTHORIZATION) token: String,
+        @RequestBody ident: Ident
+    ): ResponseEntity<*> {
+        tilgangskontrollService.harVeilederTilgangTilTjenesten(token, "/fodselsnummer", HttpMethod.POST)
         val fnr = ident.fnr.trim()
         if (fnr.isEmpty()) {
             log.error("Request mangler fnr")
--- src/test/kotlin/no/nav/sosialhjelp/modia/fodselsnummer/FodselsnummerControllerTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/modia/fodselsnummer/FodselsnummerControllerTest.kt
@@ -2,18 +2,24 @@ package no.nav.sosialhjelp.modia.fodselsnummer
 
 import io.mockk.clearAllMocks
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.runs
+import no.nav.sosialhjelp.modia.app.exceptions.ManglendeModiaSosialhjelpTilgangException
+import no.nav.sosialhjelp.modia.tilgang.TilgangskontrollService
 import no.nav.sosialhjelp.modia.utils.Ident
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpStatus
 
 internal class FodselsnummerControllerTest {
 
+    private val tilgangskontrollService: TilgangskontrollService = mockk()
     private val fodselsnummerService: FodselsnummerService = mockk()
     private val modiaBaseUrl = "http://localhost:3000/sosialhjelp/modia"
-    private val controller = FodselsnummerController(fodselsnummerService, modiaBaseUrl)
+    private val controller = FodselsnummerController(tilgangskontrollService, fodselsnummerService, modiaBaseUrl)
 
     private val ident = Ident("11111111111")
     private val fnrId = "abfcc2e8-9986-48c0-9952-eb6b724df6ce"
@@ -21,12 +27,14 @@ internal class FodselsnummerControllerTest {
     @BeforeEach
     internal fun setUp() {
         clearAllMocks()
+
+        every { tilgangskontrollService.harVeilederTilgangTilTjenesten(any(), any(), any()) } just runs
     }
 
     @Test
     fun `setFodselsnummer - skal lagre fodselsnummer og returnere modia url med fodselsnummer id`() {
         every { fodselsnummerService.setFnrForSalesforce(ident.fnr) } returns fnrId
-        val response = controller.setFodselsnummer(ident)
+        val response = controller.setFodselsnummer("token", ident)
 
         assertThat(response.body).isNotNull
         assertThat(response.body).isInstanceOf(SetFodselsnummerResponse::class.java)
@@ -36,10 +44,20 @@ internal class FodselsnummerControllerTest {
 
     @Test
     fun `setFodselsnummer - sette med tomt string og returnere bad request`() {
-        val response = controller.setFodselsnummer(Ident(" "))
+        val response = controller.setFodselsnummer("token", Ident(" "))
 
         assertThat(response.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
         assertThat(response.body).isEqualTo("Mangler fødselsnummer!")
+    }
+
+    @Test
+    fun `setFodselsnummer - ikke tilgang`() {
+        every {
+            tilgangskontrollService.harVeilederTilgangTilTjenesten(any(), any(), any())
+        } throws ManglendeModiaSosialhjelpTilgangException("veileder har ikke tilgang")
+
+        assertThatExceptionOfType(ManglendeModiaSosialhjelpTilgangException::class.java)
+            .isThrownBy { controller.setFodselsnummer("token", ident) }
     }
 
     @Test
