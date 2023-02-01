8a889794e2907e93fd6a18c8a7d34c538583b78b
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2023-02-01 09:30:52 +0100

    asd

--- src/main/kotlin/no/nav/sosialhjelp/modia/app/metrics/ExtendedClientRequestObservationConvention.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/app/metrics/ExtendedClientRequestObservationConvention.kt
@@ -1,9 +1,11 @@
 package no.nav.sosialhjelp.modia.app.metrics
 
 import io.micrometer.common.KeyValues
+import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.ClientRequestObservationContext
 import org.springframework.web.reactive.function.client.DefaultClientRequestObservationConvention
 
+@Component
 class ExtendedClientRequestObservationConvention : DefaultClientRequestObservationConvention() {
 
     override fun getLowCardinalityKeyValues(context: ClientRequestObservationContext): KeyValues {
