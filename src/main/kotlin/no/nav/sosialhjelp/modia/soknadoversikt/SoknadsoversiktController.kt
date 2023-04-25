b1863bda408232f69a2d3baf38479844f0163e44
jorel <jorgen.elden.lindgren@nav.no>
2023-04-25 15:40:21 +0200

    wip

--- src/main/kotlin/no/nav/sosialhjelp/modia/soknadoversikt/SoknadsoversiktController.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/soknadoversikt/SoknadsoversiktController.kt
@@ -108,7 +108,7 @@ class SoknadsoversiktController(
             .any { vilkar -> vilkar.status == OppgaveStatus.RELEVANT }
     }
 
-    private fun erPapirSoknad(originalSoknadNAV: OriginalSoknadNAV?): Boolean {
+    fun erPapirSoknad(originalSoknadNAV: OriginalSoknadNAV?): Boolean {
 
         if (originalSoknadNAV == null) {
             return true
@@ -116,19 +116,22 @@ class SoknadsoversiktController(
         return false
     }
 
-    private fun papirSoknadDato(saken: DigisosSak): LocalDate? {
+    //fun papirSoknadDato(saken: DigisosSak): LocalDate? {
+    fun papirSoknadDato(saken: DigisosSak): String {
 
-        if (erPapirSoknad((saken.originalSoknadNAV))) {
-
+        if (erPapirSoknad(saken.originalSoknadNAV)) { // HVIS PAPIRSØKNAD GÅ INN HER
             val model = eventService.createModel(saken)
             val saksdato = model.saker.takeIf { it.isNotEmpty() }?.get(0)?.datoOpprettet
-            if (saksdato != null) {
-                return saksdato
-            } else {
-                return model.historikk.takeIf { it.isNotEmpty() }?.get(0)?.tidspunkt?.toLocalDate()
+            if (saksdato != null) { // OM PAPIRSØKNAD HAR SAK GÅ INN HER
+                //return saksdato
+                return "saksDato"
+            } else { //OM PAPIRSØKNAD HAR INGEN SAK GÅ INN HER
+                //return model.historikk.takeIf { it.isNotEmpty() }?.get(0)?.tidspunkt?.toLocalDate()
+                return "hendelseDato"
             }
-        } else {
-            return null
+        } else {//HVIS IKKE PAPIRSØKNAD GÅ INN HER
+            //return null
+            return "sendtDato"
         }
     }
 
--- src/test/kotlin/no/nav/sosialhjelp/modia/soknadoversikt/SoknadsoversiktControllerTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/modia/soknadoversikt/SoknadsoversiktControllerTest.kt
@@ -15,8 +15,11 @@ import no.nav.sosialhjelp.modia.digisossak.domain.Sak
 import no.nav.sosialhjelp.modia.digisossak.domain.SaksStatus
 import no.nav.sosialhjelp.modia.digisossak.domain.SoknadsStatus
 import no.nav.sosialhjelp.modia.digisossak.domain.Utbetaling
+import no.nav.sosialhjelp.modia.digisossak.domain.UtfallVedtak
+import no.nav.sosialhjelp.modia.digisossak.domain.Vedtak
 import no.nav.sosialhjelp.modia.digisossak.domain.Vilkar
 import no.nav.sosialhjelp.modia.digisossak.event.EventService
+import no.nav.sosialhjelp.modia.digisossak.event.SAK_DEFAULT_TITTEL
 import no.nav.sosialhjelp.modia.digisossak.fiks.FiksClient
 import no.nav.sosialhjelp.modia.soknad.dokumentasjonkrav.DokumentasjonkravResponse
 import no.nav.sosialhjelp.modia.soknad.dokumentasjonkrav.DokumentasjonkravService
@@ -29,6 +32,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpStatus
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 internal class SoknadsoversiktControllerTest {
@@ -44,18 +48,22 @@ internal class SoknadsoversiktControllerTest {
     private val digisosSak1: DigisosSak = mockk()
     private val digisosSak2: DigisosSak = mockk()
     private val digisosSak3: DigisosSak = mockk()
+    private val digisosSak4: DigisosSak = mockk()
 
     private val model1: InternalDigisosSoker = mockk()
     private val model2: InternalDigisosSoker = mockk()
     private val model3: InternalDigisosSoker = mockk()
+    private val model4: InternalDigisosSoker = mockk()
 
     private val sak1: Sak = mockk()
     private val sak2: Sak = mockk()
     private val sak3: Sak = mockk()
+    private val sak4: Sak = mockk()
 
     private val utbetaling1: Utbetaling = mockk()
     private val utbetaling2: Utbetaling = mockk()
     private val utbetaling3: Utbetaling = mockk()
+    private val utbetaling4: Utbetaling = mockk()
 
     private val oppgaveResponseMock: OppgaveResponse = mockk()
     private val dokumentasjonkravMock: DokumentasjonkravResponse = mockk()
@@ -64,6 +72,7 @@ internal class SoknadsoversiktControllerTest {
     private val id_1 = "123"
     private val id_2 = "456"
     private val id_3 = "789"
+    private val id_4 = "101"
 
     @BeforeEach
     internal fun setUp() {
@@ -75,20 +84,87 @@ internal class SoknadsoversiktControllerTest {
         every { digisosSak1.sistEndret } returns 0L
         every { digisosSak1.digisosSoker } returns null
         every { digisosSak1.originalSoknadNAV } returns null
+        every { eventService.createModel(digisosSak1) } returns model1
 
         every { digisosSak2.fiksDigisosId } returns id_2
         every { digisosSak2.sistEndret } returns 1000L
         every { digisosSak2.digisosSoker } returns mockk()
         every { digisosSak2.originalSoknadNAV?.timestampSendt } returns System.currentTimeMillis()
+        every { eventService.createModel(digisosSak1) } returns model2
 
         every { digisosSak3.fiksDigisosId } returns id_3
         every { digisosSak3.sistEndret } returns 2000L
         every { digisosSak3.digisosSoker } returns mockk()
         every { digisosSak3.originalSoknadNAV?.timestampSendt } returns System.currentTimeMillis()
+        every { eventService.createModel(digisosSak1) } returns model3
+
+        every { digisosSak4.fiksDigisosId } returns id_4
+        every { digisosSak4.sistEndret } returns 2000L
+        every { digisosSak4.digisosSoker } returns mockk()
+        every { digisosSak4.originalSoknadNAV } returns null
+        every { eventService.createModel(digisosSak4) } returns model4
 
         every { oppgaveService.hentOppgaver(id_1) } returns listOf(oppgaveResponseMock, oppgaveResponseMock) // 2 oppgaver
         every { oppgaveService.hentOppgaver(id_2) } returns listOf(oppgaveResponseMock) // 1 oppgave
         every { dokumentasjonkravService.hentDokumentasjonkrav(id_3) } returns listOf(dokumentasjonkravMock) // 1 oppgave
+        every { dokumentasjonkravService.hentDokumentasjonkrav(id_4) } returns listOf(dokumentasjonkravMock) // 1 oppgave
+
+        every {model1.saker} returns mutableListOf()
+
+        every {model2.saker} returns mutableListOf(Sak(
+            referanse = "vanlig",
+            saksStatus = SaksStatus.UNDER_BEHANDLING,
+            tittel = SAK_DEFAULT_TITTEL,
+            vedtak = mutableListOf(
+                Vedtak(
+                    utfall = UtfallVedtak.DELVIS_INNVILGET,
+                    datoFattet = LocalDate.now().plusDays(2)
+                )
+            ),
+            utbetalinger = mutableListOf(),
+            datoOpprettet = LocalDate.now().minusDays(2)
+        ))
+
+        every {model3.saker} returns mutableListOf(Sak(
+            referanse = "vanlig",
+            saksStatus = SaksStatus.UNDER_BEHANDLING,
+            tittel = SAK_DEFAULT_TITTEL,
+            vedtak = mutableListOf(
+                Vedtak(
+                    utfall = UtfallVedtak.DELVIS_INNVILGET,
+                    datoFattet = LocalDate.now().plusDays(2)
+                )
+            ),
+            utbetalinger = mutableListOf(),
+            datoOpprettet = LocalDate.now().minusDays(2)
+        ))
+
+        every {model4.saker} returns mutableListOf(
+            Sak(
+                referanse = "papirsøknad med 2 sak",
+                saksStatus = SaksStatus.UNDER_BEHANDLING,
+                tittel = SAK_DEFAULT_TITTEL,
+                vedtak = mutableListOf(
+                    Vedtak(
+                        utfall = UtfallVedtak.INNVILGET,
+                        datoFattet = LocalDate.now().minusDays(2)
+                    ),
+                    Vedtak(
+                        utfall = UtfallVedtak.DELVIS_INNVILGET,
+                        datoFattet = LocalDate.now().plusDays(2)
+                    )
+                ),
+                utbetalinger = mutableListOf(),
+                datoOpprettet = LocalDate.now().minusDays(2)
+            ),
+            Sak(
+                referanse = "referanse",
+                saksStatus = SaksStatus.IKKE_INNSYN,
+                tittel = SAK_DEFAULT_TITTEL,
+                vedtak = mutableListOf(),
+                utbetalinger = mutableListOf(),
+                datoOpprettet = LocalDate.now().plusDays(2)
+            ))
     }
 
     @Test
@@ -260,4 +336,47 @@ internal class SoknadsoversiktControllerTest {
 
         assertThat(sak?.harOppgaver).isFalse
     }
+
+    @Test
+    fun `papirSoknadDato - hvis papirsøknad, ingen sak, vis søknadsdato (første element i historikk)`() {
+        every { fiksClient.hentAlleDigisosSaker(any()) } returns listOf(digisosSak1, digisosSak2, digisosSak3, digisosSak4)
+
+        assertThat(digisosSak1).isNotNull
+        assertThat(digisosSak1.originalSoknadNAV).isNull() // papirsøknad, skal ha 0 sak
+
+        assertThat(digisosSak2.originalSoknadNAV).isNotNull // vannlig søknad
+
+        assertThat(model1).isNotNull
+        assertThat(model1.saker).isNotNull
+        assertThat(model1.saker).isEmpty()
+        assertThat(model2.saker).isNotEmpty
+
+        val dato1 = controller.papirSoknadDato(digisosSak1) //papirsøknad, ingen sak
+        val dato2 = controller.papirSoknadDato(digisosSak2) //vanlig søknad
+        val dato3 = controller.papirSoknadDato(digisosSak3) //vanlig søknad
+        val dato4 = controller.papirSoknadDato(digisosSak4) //papirsøknad, 2 sak
+
+
+        assertThat(dato1).isEqualTo("hendelseDato") //får "saksDato", vises at det er en sak her? hvordan?
+        assertThat(dato2).isEqualTo("sendtDato")
+        assertThat(dato3).isEqualTo("sendtDato")
+        assertThat(dato4).isEqualTo("saksDato")
+    }
+
+
+    @Test
+    fun `papirSoknadDato - hvis papirsøknad, 1 sak, vis saksdato`() {
+
+    }
+
+
+    @Test
+    fun `papirSoknadDato - hvis papirsøknad, 2 sak, vis saksdato til sak 1`() {
+
+    }
+
+
+
+
+
 }
