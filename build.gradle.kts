60471b4b3fea916ce6e6bc8156867a32fa32d43d
Amund Sørumshagen <3726993+amundsor@users.noreply.github.com>
2023-07-19 12:52:21 +0200

    Dependabot version updates (#393)

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,11 +39,11 @@ object Versions {
 }
 
 plugins {
-    kotlin("jvm") version "1.8.10"
-    kotlin("plugin.spring") version "1.8.10"
-    id("org.springframework.boot") version "3.0.2"
-    id("com.github.ben-manes.versions") version "0.45.0" // ./gradlew dependencyUpdates
-    id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
+    id("org.springframework.boot") version "3.1.1"
+    id("com.github.ben-manes.versions") version "0.47.0" // ./gradlew dependencyUpdates
+    id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
 }
 
 java {
