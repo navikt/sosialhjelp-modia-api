78e83d9053ed57b027722661b040d2f58458a8eb
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-04-30 14:05:42 +0200

    Oppgradere java-versjon pluss Spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 ktlint {
@@ -90,7 +90,7 @@ repositories {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
