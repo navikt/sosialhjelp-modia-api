1ef06da1c898dac599a28638fd0e3b3fe8e4898f
joeldlin <jorgen.elden.lindgren@nav.no>
2026-01-09 10:15:53 +0100

    migrerer fra fss til gcp

--- src/main/kotlin/no/nav/sosialhjelp/modia/app/client/ProxiedHttpClientConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/app/client/ProxiedHttpClientConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import reactor.netty.http.client.HttpClient
 
-@Profile("!(dev|mock-alt|local)")
+@Profile("prod-fss|dev-fss")
 @Configuration
 class ProxiedHttpClientConfig {
     @Value("\${HTTPS_PROXY}")
@@ -16,7 +16,7 @@ class ProxiedHttpClientConfig {
     fun proxiedHttpClient(): HttpClient = proxiedHttpClient(proxyUrl)
 }
 
-@Profile("dev|mock-alt|local")
+@Profile("!(prod-fss|dev-fss)")
 @Configuration
 class MockProxiedHttpClientConfig {
     @Bean
