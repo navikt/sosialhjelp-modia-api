12510e92c0bdc6ef7fc9f8e58f574bb757233a57
Amund Sørumshagen <3726993+amundsor@users.noreply.github.com>
2026-01-13 15:16:04 +0100

    Revert "Oppgradering av Spring Boot (#592)" (#593)
    This reverts commit 1f1ee4545c0b4b5f659c9d46f965ab9f077fc2d9.

--- src/main/kotlin/no/nav/sosialhjelp/modia/redis/RedisConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/redis/RedisConfig.kt
@@ -2,18 +2,18 @@ package no.nav.sosialhjelp.modia.redis
 
 import io.lettuce.core.RedisClient
 import io.lettuce.core.RedisURI
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.boot.data.redis.autoconfigure.DataRedisProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
 @Profile("!no-redis")
 @Configuration
-@EnableConfigurationProperties(DataRedisProperties::class)
+@EnableConfigurationProperties(RedisProperties::class)
 class RedisConfig {
     @Bean
-    fun redisClient(properties: DataRedisProperties): RedisClient {
+    fun redisClient(properties: RedisProperties): RedisClient {
         val redisUri =
             RedisURI.Builder
                 .redis(properties.host, properties.port)
