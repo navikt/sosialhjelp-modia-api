e28fa21c4fb552370ce1b5f9c5305f3970e1fac7
Martin Almvik <martin.almvik@nav.no>
2026-01-15 15:18:14 +0100

    Fiks ktlint

--- src/main/kotlin/no/nav/sosialhjelp/modia/utils/MiljoUtils.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/utils/MiljoUtils.kt
@@ -1,6 +1,5 @@
 package no.nav.sosialhjelp.modia.utils
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 
