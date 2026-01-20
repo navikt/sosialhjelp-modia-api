f923476e66e83be213ceba062a2b9ee6df280911
Martin Almvik <martin.almvik@nav.no>
2026-01-20 13:38:48 +0100

    Fiks ktlint

--- src/main/kotlin/no/nav/sosialhjelp/modia/login/LoginController.kt
+++ src/main/kotlin/no/nav/sosialhjelp/modia/login/LoginController.kt
@@ -3,13 +3,13 @@ package no.nav.sosialhjelp.modia.login
 import jakarta.servlet.http.HttpServletResponse
 import no.nav.security.token.support.core.api.Unprotected
 import no.nav.sosialhjelp.modia.logger
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.util.MultiValueMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.MediaType
-import org.springframework.util.MultiValueMap
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
@@ -26,9 +26,18 @@ class LoginController(
 
     @GetMapping("/login")
     fun login(response: HttpServletResponse): ResponseEntity<LoginResponse> {
-        val responseEntity = webClient.post().uri(introEndpoint).contentType(MediaType.APPLICATION_FORM_URLENCODED).body(
-            BodyInserters.fromFormData(MultiValueMap.fromMultiValue(mapOf("token" to listOf("token"), "identity_provider" to listOf("fuck"))))
-        ).retrieve().bodyToMono<String>().block()
+        val responseEntity =
+            webClient
+                .post()
+                .uri(introEndpoint)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(
+                    BodyInserters.fromFormData(
+                        MultiValueMap.fromMultiValue(mapOf("token" to listOf("token"), "identity_provider" to listOf("fuck"))),
+                    ),
+                ).retrieve()
+                .bodyToMono<String>()
+                .block()
 
         log.info(responseEntity.toString())
         return ResponseEntity.ok(LoginResponse("ok"))
